8b97d3cbf3c61cff3076548d027728847e1be9e5
Shayn Coffey <Shayncoffey30@gmail.com>
2024-06-29 11:55:59 -0400

    completed excercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //int answer = 4; //explicit typing
+            //string response; //explicit typing
+
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + " is greater than or equal to nine";
+            //}
+
+            var answer = 4;
+            var responce = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+
+            Console.WriteLine(responce);
         }
     }
 }
